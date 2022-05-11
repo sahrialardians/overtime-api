70dc809101909f1876e8e12858278478bdecf5ac
sahrial <sahrialardians@gmail.com>
2022-05-11 12:58:16 +0700

    Employee: create service provider and register employee

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\EmployeeRepository;
+use App\Repositories\EmployeeInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(EmployeeInterface::class, EmployeeRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
