ecc37345d8ee272e6601d4eee022ea224d67d662
sahrial <sahrialardians@gmail.com>
2022-05-11 14:12:45 +0700

    implement service layer

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,33 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\EmployeeRequest;
-use App\Repositories\EmployeeInterface;
+use App\Services\EmployeeServiceInterface;
 
 class EmployeeController extends Controller
 {
-    protected $employeeRepository;
+    protected $employeeService;
 
-    public function __construct(EmployeeInterface $employeeInterface)
+    public function __construct(EmployeeServiceInterface $employeeServiceInterface)
     {
-        $this->employeeRepository = $employeeInterface;
+        $this->employeeService = $employeeServiceInterface;
     }
 
     public function store(EmployeeRequest $request)
     {    
-        try {
-            $this->employeeRepository->save($request->validated());
-
-            return response()->json([
-                'code' => 201,
-                'status' => true,
-                'message' => 'Succesfully created employee.'
-            ], 201);
-        } catch (\Exception $e) {
-            return response()->json([
-                'code' => 500,
-                'status' => false,
-                'message' => $e->getMessage()
-            ], 500);
-        }
+        return $this->employeeService->save($request->validated());
     }
 }
