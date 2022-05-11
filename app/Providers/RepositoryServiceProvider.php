febc2c6253e96ab20f7954870d3930405cee2cfb
sahrial <sahrialardians@gmail.com>
2022-05-11 14:14:20 +0700

    register service layer to repository providers

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\EmployeeRepository;
 use App\Repositories\EmployeeInterface;
+use App\Services\EmployeeService;
+use App\Services\EmployeeServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(EmployeeInterface::class, EmployeeRepository::class);
+        $this->app->bind(EmployeeServiceInterface::class, EmployeeService::class);
     }
 
     /**
