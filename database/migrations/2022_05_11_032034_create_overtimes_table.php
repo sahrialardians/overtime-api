ea43a1899cf1acf33162fca0eb9c26569a91d1e7
sahrial <sahrialardians@gmail.com>
2022-05-11 10:32:56 +0700

    setup tables

---
+++ database/migrations/2022_05_11_032034_create_overtimes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOvertimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('overtimes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('employees')->onDelete('cascade');
+            $table->date('date');
+            $table->time('time_started')->nullable();
+            $table->time('time_ended')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('overtimes');
+    }
+}
